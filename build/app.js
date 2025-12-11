0b14bb8ceb5b5fe0f6cf6bd829c72d87db5d199a
Madhumita Das <madhumita011das@gmail.com>
2025-12-11 17:38:12 +0530

    add files

---
+++ build/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Madhumita";
+console.log(name);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,2 @@
+const name: string = "Madhumita"
+console.log(name)
